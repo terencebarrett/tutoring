3be9f5fdee82ad0d8ca25472d9b57b6406b75b5b
terencebarrett <terence@vetrofibermap.com>
2020-09-13 13:40:14 -0400

    before third meeting

--- future_topics.py
+++ future_topics.py
@@ -35,18 +35,3 @@
 
 
 
-# Build on the Challenge in Second with:
-
-# grade >= 'C'
-
-# input()
-# grade_str = input('Enter grade:')
-# grade_int = int(grade_str)
-
-# loops
-# for loop that runs a list of grades collected one at a time with input() or with a list sent to a function
-# while loop for grabbing grades with input() until there is no more
-
-# dictionary of student names and grades
-
-# average grade of the students, histogram of grades, etc
---
+++ third.py
@@ -0,0 +1,15 @@
+# Build on the Challenge in Second with:
+
+# grade >= 'C' -or- grade in ['A', 'B', 'C']
+
+# input()
+# grade_str = input('Enter grade:')
+# grade_int = int(grade_str)
+
+# loops
+# for loop that runs a list of grades collected one at a time with input() or with a list sent to a function
+# while loop for grabbing grades with input() until there is no more
+
+# dictionary of student names and grades
+
+# average grade of the students, histogram of grades, etc
