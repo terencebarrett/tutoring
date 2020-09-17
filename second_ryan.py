525c27aa693d1079a85ff69c2d2cbcdd5ed14ed3
terencebarrett <terence@vetrofibermap.com>
2020-09-17 14:36:42 -0400

    for loop

--- second_ryan.py
+++ second_ryan.py
@@ -7,18 +7,24 @@ string_6 = "A."
 string_7 = "*Note: Student needs extra help"
 string_8 = "error"
 
-x = 65
 
-if 59 >= x >= 0:
-    print(string_1, string_2, string_7)
-elif 69 >= x >= 60:
-    print(string_1, string_3, string_7)
-elif 79 >= x >= 70:
-    print(string_1, string_4, string_7)
-elif 89 >= x >= 80:
-    print (string_1, string_5)
-elif 100 >= x >= 90:
-    print(string_1, string_6)
-else:
-    print(string_1, string_8)
+for i in [1, 2, 3]:
+
+    grade_str = input(f'Enter grade for student# {i}: ')
+    grade_int = int(grade_str)
+
+    if 59 >= grade_int >= 0:
+        print(string_1, string_2, string_7)
+    elif 69 >= grade_int >= 60:
+        print(string_1, string_3, string_7)
+    elif 79 >= grade_int >= 70:
+        print(string_1, string_4, string_7)
+    elif 89 >= grade_int >= 80:
+        print(string_1, string_5)
+    elif 100 >= grade_int >= 90:
+        print(string_1, string_6)
+    else:
+        print(string_1, string_8)
+
+print('All done!\n')
 
