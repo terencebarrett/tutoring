79abeee09a43687a58510d69221a2da39c25e954
terencebarrett <terence@vetrofibermap.com>
2020-09-17 14:25:06 -0400

    clean up of hw

--- second_ryan.py
+++ second_ryan.py
@@ -7,23 +7,18 @@ string_6 = "A."
 string_7 = "*Note: Student needs extra help"
 string_8 = "error"
 
-x = 77
+x = 65
 
-if x <= 59 and x >= 0:
-    print (string_1, string_2, string_7)
-
-if x <= 69 and x >= 60:
-    print (string_1, string_3, string_7)
-
-if x <= 79 and x >= 70:
-    print (string_1, string_4, string_7)
-
-if x <= 89 and x >= 80:
+if 59 >= x >= 0:
+    print(string_1, string_2, string_7)
+elif 69 >= x >= 60:
+    print(string_1, string_3, string_7)
+elif 79 >= x >= 70:
+    print(string_1, string_4, string_7)
+elif 89 >= x >= 80:
     print (string_1, string_5)
-
-if x <= 100 and x >= 90:
-    print (string_1, string_6)
-
-if x >= 100 or x <= 0:
-    print (string_1, string_8)
+elif 100 >= x >= 90:
+    print(string_1, string_6)
+else:
+    print(string_1, string_8)
 
