e717293363dd31029a961b7560192a132dee138c
terencebarrett <terence@vetrofibermap.com>
2020-09-17 15:19:22 -0400

    while loop

--- second_ryan.py
+++ second_ryan.py
@@ -7,24 +7,30 @@ string_6 = "A."
 string_7 = "*Note: Student needs extra help"
 string_8 = "error"
 
-
-for i in [1, 2, 3]:
-
-    grade_str = input(f'Enter grade for student# {i}: ')
-    grade_int = int(grade_str)
-
-    if 59 >= grade_int >= 0:
-        print(string_1, string_2, string_7)
-    elif 69 >= grade_int >= 60:
-        print(string_1, string_3, string_7)
-    elif 79 >= grade_int >= 70:
-        print(string_1, string_4, string_7)
-    elif 89 >= grade_int >= 80:
-        print(string_1, string_5)
-    elif 100 >= grade_int >= 90:
-        print(string_1, string_6)
+keep_going = True
+i = 0
+while keep_going:
+    i += 1
+    input_str = input(f'Enter grade for student# {i} or "quit" to stop grading: ')
+    if input_str == 'quit':
+        keep_going = False
+    elif not input_str.isdigit():
+        print('Incorrect input!')
+        i -= 1
     else:
-        print(string_1, string_8)
+        grade_int = int(input_str)
+        if 59 >= grade_int >= 0:
+            print(string_1, string_2, string_7)
+        elif 69 >= grade_int >= 60:
+            print(string_1, string_3, string_7)
+        elif 79 >= grade_int >= 70:
+            print(string_1, string_4, string_7)
+        elif 89 >= grade_int >= 80:
+            print(string_1, string_5)
+        elif 100 >= grade_int >= 90:
+            print(string_1, string_6)
+        else:
+            print(string_1, string_8)
 
 print('All done!\n')
 
