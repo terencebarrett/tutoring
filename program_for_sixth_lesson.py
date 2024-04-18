0cf4e356b3e1a271060fb35a9ff6bc5ceafdf515
tcbarret <tcbarret@uvm.edu>
2024-04-18 10:07:47 -0400

    Fix line length

--- program_for_sixth_lesson.py
+++ program_for_sixth_lesson.py
@@ -13,7 +13,8 @@ i = 0
 grade_dict = {}
 while keep_going:
     i += 1
-    input_str = input('Enter student name and their numerical grade (with a space between) -- or "quit" to finish: ')
+    input_str = input('Enter student name and their numerical grade (with a '
+                      'space between) -- or "quit" to finish: ')
     input_list = input_str.split()
     if 'quit' in input_str:
         keep_going = False
@@ -35,10 +36,12 @@ else:
     names = list(grade_dict.keys())
     number_grades = []
     for name in names:
-        print(name, '\t', 'number grade:', grade_dict[name]['number_grade'], ',', grade_dict[name]['msg'])
+        print(name, '\t', 'number grade:', grade_dict[name]['number_grade'],
+              ',', grade_dict[name]['msg'])
         number_grades.append(grade_dict[name]['number_grade'])
     average_grade = round(mean(number_grades))
-    print('The average grade is', average_grade, ',', msg_from_number_grade(average_grade, is_student=False))
+    print('The average grade is', average_grade, ',',
+          msg_from_number_grade(average_grade, is_student=False))
 
 # Plot the student grades
 fig_1 = plt.figure()
@@ -47,8 +50,8 @@ plt.plot(names, [average_grade] * len(names), 'r:', label='Class Average')
 plt.xlabel('Student')
 plt.ylabel('Grade')
 plt.legend()
-plt.title(f'Test results for the {len(names)} students (class average: {average_grade})')
+plt.title(f'Test results for the {len(names)} students '
+          f'(class average: {average_grade})')
 plt.grid()
 plt.savefig('plots/grades.png')
 plt.show()
-
